9151313ca5d3da582cda5592ee4d09d5388b688a
NICO <nico19@ukr.net>
2015-05-05 10:00:28 +0300

    завантажую тести для перевірки базових операцій над балансом

---
+++ UnitTestPAccaunt.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using dataLib;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTestPAccaunt
+    {
+        // unit test code
+        [TestMethod]
+        public void DecreaseBalance_WithValidAmount_UpdatesBalance()
+        {
+            // arrange
+            Int32 userId = 1;
+            double beginningBalance = PAccaunt.GetBalance(userId);
+            double decreaseAmount = 4.55;
+            double expected = 7.44;
+            PAccaunt account = new PAccaunt(userId);
+
+            // act
+            account.DecreaseBalance(decreaseAmount);
+
+            // assert
+            double actual = PAccaunt.GetBalance(userId);
+            Assert.AreEqual(expected, actual, 0.001, "Account not decreased correctly");
+        }
+
+        [TestMethod]
+        public void IncreaseBalance_WithValidAmount_UpdatesBalance()
+        {
+            // arrange
+            Int32 userId = 1;
+            double beginningBalance = PAccaunt.GetBalance(userId);
+            double increaseAmount = 4.55;
+            double expected = 11.99;
+            PAccaunt account = new PAccaunt(userId);
+
+            // act
+            account.IncreaseBalance(increaseAmount);
+
+            // assert
+            double actual = PAccaunt.GetBalance(userId);
+            Assert.AreEqual(expected, actual, 0.001, "Account not decreased correctly");
+        }
+    }
+}
